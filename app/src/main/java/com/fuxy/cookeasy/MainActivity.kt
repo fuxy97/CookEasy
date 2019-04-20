a9643cf33890592dfb6c43008d6e8147a801d73d
Roman Mazalov <dellight55@gmail.com>
2019-04-20 16:59:13 +0300

    Add dependencies and migrate project to AndroidX
    * Refactor project to AndroidX
    * Add dependecies for Room library in build.gradle

--- app/src/main/java/com/fuxy/cookeasy/MainActivity.kt
+++ app/src/main/java/com/fuxy/cookeasy/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.fuxy.cookeasy
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
