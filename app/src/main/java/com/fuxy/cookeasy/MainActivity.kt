5b85a0399646195f6412a89363711cc47ba552d1
Roman Mazalov <dellight55@gmail.com>
2019-04-21 19:25:17 +0300

    Add singleton with Amazon S3 credentials
    * Add JSON file with Amazon S3 credential in resources
    * Create SingletonHolder generic class
    * Create AmazonS3Credentials singleton class

--- app/src/main/java/com/fuxy/cookeasy/MainActivity.kt
+++ app/src/main/java/com/fuxy/cookeasy/MainActivity.kt
@@ -2,11 +2,19 @@ package com.fuxy.cookeasy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.fuxy.cookeasy.s3.AmazonS3Credentials
 
 class MainActivity : AppCompatActivity() {
+
+    private var helloWorldTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloWorldTextView = findViewById(R.id.tv_hello_world)
+
+        helloWorldTextView?.text = AmazonS3Credentials.getInstance(applicationContext).awsService
     }
 }
---
+++ app/src/main/java/com/fuxy/cookeasy/s3/AmazonS3Credentials.kt
@@ -0,0 +1,27 @@
+package com.fuxy.cookeasy.s3
+
+import android.content.Context
+import com.fuxy.cookeasy.R
+import com.fuxy.cookeasy.SingletonHolder
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import java.io.InputStreamReader
+
+class AmazonS3Credentials private constructor(context: Context) {
+    val awsService: String
+    val awsRegion: String
+    val accessKeyId: String
+    val secretAccessKey: String
+
+    init {
+        val inputStream = context.resources.openRawResource(R.raw.s3_credentials)
+        val jsonObject = JsonParser().parse(InputStreamReader(inputStream, "UTF-8")) as JsonObject
+        val s3CredentialsObject = jsonObject.getAsJsonObject("s3_credentials")
+        awsService = s3CredentialsObject.get("aws_service").asString
+        awsRegion = s3CredentialsObject.get("aws_region").asString
+        accessKeyId = s3CredentialsObject.get("access_key_id").asString
+        secretAccessKey = s3CredentialsObject.get("secret_access_key").asString
+    }
+
+    companion object : SingletonHolder<AmazonS3Credentials, Context>(::AmazonS3Credentials)
+}
